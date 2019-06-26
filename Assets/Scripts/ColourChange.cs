7d29eb71b0742992f0a684d5bc6fccea9a89bce7
Daniel <wilson51@live.com>
2019-06-26 19:16:22 +1000

    Basic assets.
    Moved basic assets and scripts from separate test project. Placed in scene,

---
+++ Assets/Scripts/ColourChange.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourChange : MonoBehaviour
+{
+    // Scroll main texture based on time
+
+    public float scrollSpeed;
+    Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        float offset = Time.time * scrollSpeed;
+        rend.material.mainTextureOffset = new Vector2(offset, 0);
+        
+    }
+
+    IEnumerator Fill()
+    {
+        //make new float = 0
+        //while float < 1
+            //fill towards 1
+            //offset texture
+            yield return null;
+    }
+}
---
+++ Assets/Scripts/RotateOnClick.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateOnClick : MonoBehaviour
+{
+   
+    // Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    //Click left mouse button to rotate target
+    private void OnMouseDown()
+    {
+        transform.Rotate(45, 0, 0);
+    }
+
+    
+}
