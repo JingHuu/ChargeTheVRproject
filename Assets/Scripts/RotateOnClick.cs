f2146bf051e326c38c8e61cd448a22b62568021a
Daniel Wilson <wilson51@live.com>
2019-07-01 11:26:00 +1000

    Duplicate Oculus VR controller test scene to main scene folder.
    Added to scripts. Tried to link up controller with mouse input.

--- Assets/Scripts/RotateOnClick.cs
+++ Assets/Scripts/RotateOnClick.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class RotateOnClick : MonoBehaviour
 {
-   
-    // Update is called once per frame
-	void Update ()
-    {
-		
-	}
+    
 
     //Click left mouse button to rotate target
     private void OnMouseDown()
     {
         transform.Rotate(45, 0, 0);
     }
-
     
+    private void WhenTriggerPulled()
+    {
+        OVRInput.Controller activeController = OVRInput.GetActiveController();
+        float indexTrigger = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger);    
+        if (indexTrigger != 0.0f)
+        {
+            transform.Rotate(45, 0, 0);
+        }
+    }
 }
