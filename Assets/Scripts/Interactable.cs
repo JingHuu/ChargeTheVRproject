4fd86dd8ad7277293ad289ef25556186dfde89fc
Jing <1013263@student.sae.edu.au>
2019-07-08 19:25:44 +1000

    trying out oculus control and fail miserably

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+
+
+    public void RotateNode()
+    {
+        transform.Rotate(45, 0, 0);
+
+        /*OVRInput.Controller activeController = OVRInput.GetActiveController();
+        float indexTrigger = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger);
+        if (indexTrigger != 0.0f)
+        {
+            transform.Rotate(45, 0, 0);
+        }*/
+    }
+
+
+}
---
+++ Assets/Scripts/Jing_PlayerEvents.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Jing_PlayerEvents : MonoBehaviour
+{
+    #region Events
+    public static UnityAction OnTouchpadUp = null;
+    public static UnityAction OnTouchpadDown = null;
+    public static UnityAction<OVRInput.Controller, GameObject> OnControllerSource = null;
+    #endregion
+
+    #region Anchors
+    public GameObject m_LeftAnchor;
+    public GameObject m_RightAnchor;
+    public GameObject m_HeadAnchor;
+    #endregion
+
+    #region Input
+    private Dictionary<OVRInput.Controller, GameObject> m_ControllerSets = null;
+    private OVRInput.Controller m_InputSource = OVRInput.Controller.None;
+    private OVRInput.Controller m_Controller = OVRInput.Controller.None;
+    private bool m_InputActive = true;
+    #endregion
+
+    private void Awake()
+    {
+        OVRManager.HMDMounted += PlayerFound;
+        OVRManager.HMDUnmounted += PlayerLoss;
+
+        m_ControllerSets = CreateControllerSets();
+    }
+
+    private void OnDestroy()
+    {
+        OVRManager.HMDMounted -= PlayerFound;
+        OVRManager.HMDUnmounted -= PlayerLoss;
+    }
+
+
+    private void Update()
+    {
+        //check for active input
+        if (!m_InputActive)
+            return;
+
+        //check if the controler exists
+        CheckForController();
+
+        //check for input source
+        CheckInputSource();
+
+        //check for actual input
+        Input();
+    }
+
+    private void CheckForController()
+    {
+        OVRInput.Controller controllerCheck = m_Controller;
+
+        //Right remote
+        if (OVRInput.IsControllerConnected(OVRInput.Controller.RTrackedRemote))
+            controllerCheck = OVRInput.Controller.RTouch;
+
+        //left remote
+        if (OVRInput.IsControllerConnected(OVRInput.Controller.LTrackedRemote))
+            controllerCheck = OVRInput.Controller.LTouch;
+
+        //If no controllers, headset
+        if (!OVRInput.IsControllerConnected(OVRInput.Controller.RTrackedRemote) && !OVRInput.IsControllerConnected(OVRInput.Controller.LTrackedRemote))
+            controllerCheck = OVRInput.Controller.Touchpad;
+
+        //update
+        m_Controller = UpdateSource(controllerCheck, m_Controller);
+    }
+
+    private void CheckInputSource()
+    {
+        //update
+        m_InputSource = UpdateSource(OVRInput.GetActiveController(), m_InputSource);
+    }
+
+    private void Input()
+    {
+        //Touchpad down
+        if(OVRInput.GetDown(OVRInput.Button.PrimaryTouchpad))
+        {
+            if (OnTouchpadDown != null)
+                OnTouchpadDown();
+        }
+
+        //Touchpad up
+        if (OVRInput.GetUp(OVRInput.Button.PrimaryTouchpad))
+        {
+            if (OnTouchpadUp != null)
+                OnTouchpadUp();
+        }
+    }
+
+    private OVRInput.Controller UpdateSource(OVRInput.Controller check, OVRInput.Controller previous)
+    {
+        //if values are the same, return
+        if (check == previous)
+            return previous;
+
+        //get controller object
+        GameObject controllerObject = null;
+        m_ControllerSets.TryGetValue(check, out controllerObject);
+
+        //if no controller object, set to the head
+        if (controllerObject == null)
+            controllerObject = m_HeadAnchor;
+
+        //Send out event
+        if (OnControllerSource != null)
+            OnControllerSource(check, controllerObject);
+
+        //Return new value
+        return check;
+    }
+
+    private void PlayerFound()
+    {
+        m_InputActive = true;
+    }
+
+    private void PlayerLoss()
+    {
+        m_InputActive = false;
+    }
+
+    private Dictionary<OVRInput.Controller, GameObject> CreateControllerSets()
+    {
+        Dictionary<OVRInput.Controller, GameObject> newSets = new Dictionary<OVRInput.Controller, GameObject>()
+        {
+            {OVRInput.Controller.LTouch, m_LeftAnchor },
+            {OVRInput.Controller.RTouch, m_RightAnchor },
+            {OVRInput.Controller.Remote, m_HeadAnchor },
+        };
+
+        return newSets;
+    }
+
+}
